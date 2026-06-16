2e851043aea8d9cd06eecef68ae806f834a7ffc0
Rohan <andriamihaja2007rohan@gmail.com>
2026-06-16 11:31:00 +0300

    fix loadingClass

--- src/main/java/roro/servlet/FrontControllerServlet.java
+++ src/main/java/roro/servlet/FrontControllerServlet.java
@@ -2,6 +2,8 @@ package roro.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -9,6 +11,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontControllerServlet extends HttpServlet {
+
+    List<String> listeClasse = new ArrayList<>();
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        String packageName = "roro.app";
+        String monAnnotation = "roro.annotation.MonController";
+        listeClasse = roro.util.LoadingClass.loadClassWithMyAnnotation(packageName, monAnnotation);
+        // listeClasse = roro.util.LoadingClass.loadAllClasses();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
@@ -26,11 +39,15 @@ public class FrontControllerServlet extends HttpServlet {
             throws IOException {
         response.setContentType("text/plain;charset=UTF-8");
 
-        String url = request.getRequestURL().toString();
+        // String url = request.getRequestURL().toString();
 
         try (PrintWriter out = response.getWriter()) {
             out.println("---Mon Framework Perso ---");
-            out.println("URL interceptée : " + url);
+            // out.println("URL interceptée : " + url);
+
+            for (String classe : listeClasse) {
+                out.println(classe);
+            }
         }
     }
 }

5e5fcc7f2fb0a5e8cd290537e6ac024e34ec9f1a
Rohan <andriamihaja2007rohan@gmail.com>
2026-06-16 10:54:33 +0300

    creation de la methide loadClass

--- src/main/java/roro/util/LoadingClass.java
+++ src/main/java/roro/util/LoadingClass.java
@@ -1,11 +1,27 @@
 package roro.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
 import roro.annotation.MonController;
 
 public class LoadingClass {
-    public static boolean hasAnnotation(Class<?> clazzScanned){
+    public static boolean hasAnnotation(Class<?> clazzScanned) {
         return clazzScanned.isAnnotationPresent(MonController.class);
     }
 
-    
+    public static List<String> loadClass(String packageName) {
+        List<String> listeClasse = new ArrayList<>();
+        try (ScanResult scanResult = new ClassGraph().enableAllInfo().acceptPackages(packageName).scan()) {
+            ClassInfoList classes = scanResult.getAllClasses();
+            for (ClassInfo classInfo : classes) {
+                listeClasse.add(classInfo.getName());
+            }
+        }
+        return listeClasse;
+    }
 }

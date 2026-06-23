62f3daae91e01bf3d3a5264e28135a472adb41f1
Rohan <andriamihaja2007rohan@gmail.com>
2026-06-23 17:31:02 +0300

    Update Mapping.java

--- src/main/java/roro/util/Mapping.java
+++ src/main/java/roro/util/Mapping.java
@@ -2,23 +2,20 @@ package roro.util;
 import java.lang.reflect.Method;
 
 public class Mapping {
-    private final String url;
     private final Class<?> controllerClass;
     private final Method method;
 
-    public Mapping(String url, Class<?> controllerClass, Method method) {
-        this.url = url;
+    public Mapping(Class<?> controllerClass, Method method) {
         this.controllerClass = controllerClass;
         this.method = method;
     }
 
-    public String getUrl() { return url; }
     public String getClassName() { return controllerClass.getName(); }
     public Method getMethod() { return method; }
     public Class<?> getControllerClass() { return controllerClass; }
 
     @Override
     public String toString() {
-        return url + " -> " + controllerClass.getName() + "." + method.getName() + "()";
+        return controllerClass.getName() + "." + method.getName() + "()";
     }
 }

07422c80d1c336c7f193cc7c997603a079b45c29
Rohan <andriamihaja2007rohan@gmail.com>
2026-09-23 12:17:14 +0300

    annotation rest

--- src/main/java/roro/annotation/Rest.java
+++ src/main/java/roro/annotation/Rest.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
-public @interface Json {
+public @interface Rest {
     String value() default "";
 }
